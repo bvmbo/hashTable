b71fb87975a6384a67f356c19b964c57213212e9
bvmbo <wiktorsz2001@wp.pl>
2023-03-08 21:55:36 +0100

    added delete option, no collisions

--- hash/hash.cpp
+++ hash/hash.cpp
@@ -20,52 +20,60 @@ int hashFun(int size, long key) {
 
 
 void insert() {
-    //int key, value, tabSize;
-    //int collisionCounter = 0;
     vector<vector<pair<long, string>>> tab;
+    
     string command, data;
     long key;
+    int cases;
 
-    while (true) {
-        cin >> command;
-        if (command == "size") {
-            cin >> key;
-            tab.resize(key);
-        }
-        else if (command == "add") {
-            cin >> key;
-            cin >> data;
+    cin >> cases;
 
-            long tmp = key;
-            bool check = false;
-            
-            while (!check) {
-                int index = hashFun(tab.size(), tmp);
-                
-                if (!tab[index].empty()) {
-                    tmp++;                   
-                }
-                else {
-                    tab[index].push_back(make_pair(key, data));
-                    check = true;
-                }
-            }                  
-        }
-        else if (command == "print") {
-            cout << endl;
-            for (int i = 0; i < tab.size(); i++) {
-                if (!tab[i].empty()) {
-                    cout << i << " " << tab[i][0].first << " " << tab[i][0].second << endl;
+    for (int i = 0; i < cases; i++) {
+        while (true) {
+            cin >> command;
+            if (command == "size") {
+                cin >> key;
+                tab.resize(key);
+            }
+            else if (command == "add") {
+                cin >> key;
+                cin >> data;
+
+                long tmp = key;
+                bool check = false;
+
+                while (!check) {
+                    int index = hashFun(tab.size(), tmp);
+
+                    if (!tab[index].empty()) {
+                        tmp++;
+                    }
+                    else {
+                        tab[index].push_back(make_pair(key, data));
+                        check = true;
+                    }
                 }
             }
-        }
-        else if (command == "delete") {
+            else if (command == "print") {
+                cout << endl;
+                for (int i = 0; i < tab.size(); i++) {
+                    if (!tab[i].empty()) {
+                        cout << i << " " << tab[i][0].first << " " << tab[i][0].second << endl;
+                    }
+                }
+                cout << endl;
+            }
+            else if (command == "delete") {
+                cin >> key;
+                int index = hashFun(tab.size(), key);
+                tab[index].clear();
 
+            }
+            else if (command == "stop") {
+                break;
+            }
         }
-        else if (command == "stop") {
-            break;
-        }
+
+        tab.clear();
     }
-
-
 };
